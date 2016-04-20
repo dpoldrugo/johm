e1127ac337cc9f30c80030304d2dd3282b70e0ad
dpoldrugo <davor.poldrugo@infobip.com>
2016-04-20 14:27:43 +0200

    added embedded redis

--- src/test/java/redis/clients/johm/JOhmTestBase.java
+++ src/test/java/redis/clients/johm/JOhmTestBase.java
@@ -1,5 +1,7 @@
 package redis.clients.johm;
 
+import java.io.IOException;
+
 import org.apache.commons.pool.impl.GenericObjectPool.Config;
 import org.junit.Assert;
 import org.junit.Before;
@@ -7,14 +9,22 @@ import org.junit.Before;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.Protocol;
+import redis.embedded.RedisServer;
 
 public class JOhmTestBase extends Assert {
     protected JedisPool jedisPool;
     protected volatile static boolean benchmarkMode;
+    private RedisServer redisServer;
 
     @Before
-    public void startUp() {
+    public void startUp() throws IOException {
+        startEmbeddedRedis();
         startJedisEngine();
+    }
+
+    protected void startEmbeddedRedis() throws IOException {
+        redisServer = new RedisServer(Protocol.DEFAULT_PORT);
+        redisServer.start();
     }
 
     protected void startJedisEngine() {
