abf1e68c373abd760708104ba9cf2a1afb5b3320
dpoldrugo <davor.poldrugo@infobip.com>
2016-04-20 14:42:05 +0200

    added random port for redis embedded

--- src/test/java/redis/clients/johm/JOhmTestBase.java
+++ src/test/java/redis/clients/johm/JOhmTestBase.java
@@ -1,6 +1,7 @@
 package redis.clients.johm;
 
 import java.io.IOException;
+import java.util.Random;
 
 import org.apache.commons.pool.impl.GenericObjectPool.Config;
 import org.junit.Assert;
@@ -8,31 +9,32 @@ import org.junit.Before;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.Protocol;
 import redis.embedded.RedisServer;
 
 public class JOhmTestBase extends Assert {
     protected JedisPool jedisPool;
     protected volatile static boolean benchmarkMode;
     private RedisServer redisServer;
+    private int redisPort;
 
     @Before
     public void startUp() throws IOException {
+        redisPort = new Random(System.currentTimeMillis()).nextInt(50000);
         startEmbeddedRedis();
         startJedisEngine();
     }
 
     protected void startEmbeddedRedis() throws IOException {
-        redisServer = new RedisServer(Protocol.DEFAULT_PORT);
+        redisServer = new RedisServer(redisPort);
         redisServer.start();
     }
 
     protected void startJedisEngine() {
         if (benchmarkMode) {
             jedisPool = new JedisPool(new Config(), "localhost",
-                    Protocol.DEFAULT_PORT, 2000);
+                    redisPort, 2000);
         } else {
-            jedisPool = new JedisPool(new Config(), "localhost");
+            jedisPool = new JedisPool(new Config(), "localhost", redisPort);
         }
         JOhm.setPool(jedisPool);
         purgeRedis();
